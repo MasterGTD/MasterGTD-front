dce01f897fc22deea80d077a724bda836581da65
Angeliz <471330418@qq.com>
2017-08-30 17:35:57 +0800

    表单验证（未测试）

--- login/j_login.js
+++ login/j_login.js
@@ -1,6 +1,6 @@
 
 window.onload = function(){
-    //切换登录和注册 法1
+    // 切换登录和注册 法1
     // var myPart1 = document.getElementById("part1");    //整个div
     // var myUl = myPart1.getElementsByTagName("ul")[0];//一个节点
     // var myLi = myUl.getElementsByTagName("li");    //数组
@@ -29,7 +29,6 @@ window.onload = function(){
     imgArr[5]="./img/Assassin3.jpg";
     imgArr[6]="./img/Assassin4.jpg";
     var index =parseInt(Math.random()*(imgArr.length)); 
-    console.log(index)
     var currentImage=imgArr[index]; 
     document.getElementsByTagName("body")[0].style="background:white url(" + currentImage + ") no-repeat;background-size:100%";  
 }
@@ -48,3 +47,73 @@ $(document).ready(function(){
     });
 });
 
+//表单验证
+$(function(){
+
+        var ok1=false;
+        var ok2=false;
+        var ok3=false;
+        var ok4=false;
+        // 验证用户名
+        $('input[name="username"]').focus(function(){
+          $(this).next().text('用户名应该为3-20位之间');
+        }).blur(function(){
+          if($(this).val().length >= 3 && $(this).val().length <=12 && $(this).val()!=''){
+            $(this).next().text('输入成功');
+            ok1=true;
+          }else{
+            $(this).next().text('用户名应该为3-20位之间');
+          }
+            
+        });
+  
+        //验证密码
+        $('input[name="password"]').focus(function(){
+          $(this).next().text('密码应该为6-20位之间');
+        }).blur(function(){
+          if($(this).val().length >= 6 && $(this).val().length <=20 && $(this).val()!=''){
+            $(this).next().text('输入成功');
+            ok2=true;
+          }else{
+            $(this).next().text('密码应该为6-20位之间');
+          }
+            
+        });
+  
+        //验证确认密码
+        //   $('input[name="repass"]').focus(function(){
+        //   $(this).next().text('输入的确认密码要和上面的密码一致,规则也要相同');
+        // }).blur(function(){
+        //   if($(this).val().length >= 6 && $(this).val().length <=20 && $(this).val()!='' && $(this).val() == $('input[name="password"]').val()){
+        //     $(this).next().text('输入成功');
+        //     ok3=true;
+        //   }else{
+        //     $(this).next().text('输入的确认密码要和上面的密码一致,规则也要相同');
+        //   }
+            
+        // });
+  
+        //验证邮箱
+        $('input[name="email"]').focus(function(){
+          $(this).next().text('请输入正确的EMAIL格式');
+        }).blur(function(){
+          if($(this).val().search(/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/)==-1){
+            $(this).next().text('请输入正确的EMAIL格式');
+          }else{         
+            $(this).next().text('输入成功');
+            ok4=true;
+          }
+            
+        });
+  
+        //提交按钮,所有验证通过方可提交
+  
+        $('.button').click(function(){
+          if(ok1 && ok2 &&  ok4){
+            $('form').submit();
+          }else{
+            return false;
+          }
+        });
+          
+    });
